75e61cdbbace27ff615625d7367530fe46643472
bhuvii08 <bhuvneshwarighartaan08@gmail.com>
2026-07-17 21:17:26 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -92,6 +92,7 @@ const configuredClientOrigins = [
 
 const allowedOrigins = [
   ...configuredClientOrigins,
+  "https://ai-interview-server-chi.vercel.app",
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
